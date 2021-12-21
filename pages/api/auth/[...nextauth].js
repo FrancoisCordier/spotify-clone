be881656a9cf6f541151acfe02a1c62de601cddc
François Cordier <francoiscordier.ece@gmail.com>
2021-12-21 23:34:12 +0100

    Login NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,27 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+const refreshAccessToken = async () => {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+
+    console.log("REFRESHED TOKEN IS", refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (e) {
+    console.error(e);
+    return { ...token, error: "RefreshAccessTokenError" };
+  }
+};
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +32,40 @@ export default NextAuth({
       authorization: LOGIN_URL,
     }),
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // Première connexion
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+
+      // Retourne le token s'il n'a pas expiré
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("TOKEN VALIDE");
+        return token;
+      }
+
+      // Le token a expiré, il faut en générer un nouveau
+      console.log("TOKEN EXPIRE, REFRESH...");
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
+  },
 });
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,28 @@
-function Login() {
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
+  console.log(providers);
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+      {Object.values(providers).map((provider) => (
+        <div>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callback: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return { props: { providers } };
+}
